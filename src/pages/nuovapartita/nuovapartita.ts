cc7cbc2cedac36f73674357a14b809d0f027eb18
franfeg <francescofegatilli@gmail.com>
2019-03-22 18:36:02 +0100

    nuova partite

---
+++ src/pages/nuovapartita/nuovapartita.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the NuovapartitaPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-nuovapartita',
+  templateUrl: 'nuovapartita.html',
+  
+})
+export class NuovapartitaPage {
+  
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+/*
+  customActionSheetOptions: any = {
+    header: 'Colors',
+    subHeader: 'Select your favorite color'
+  }*/
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad NuovapartitaPage');
+  }
+
+}

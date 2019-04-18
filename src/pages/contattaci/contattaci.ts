38c760f96fd0143703724079d3303e8f84afd74a
franfeg <francescofegatilli@gmail.com>
2019-04-18 17:10:17 +0200

    no mail composer

--- src/pages/contattaci/contattaci.ts
+++ src/pages/contattaci/contattaci.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-import {EmailComposer} from '@ionic-native/email-composer/ngx';
+
 /**
  * Generated class for the ContattaciPage page.
  *
@@ -16,11 +16,8 @@ import {EmailComposer} from '@ionic-native/email-composer/ngx';
 })
 export class ContattaciPage {
 
-  subject='';
-  body='';
-  to='';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public emailComposer: EmailComposer,) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,) {
 
   }
 
@@ -28,19 +25,5 @@ export class ContattaciPage {
     console.log('ionViewDidLoad ContattaciPage');
   }
 
-  send(){
-    let email = {
-      to: this.to,
-      cc: [],
-      bcc: [],
-      attachments: [],
-      subject: this.subject,
-      body: this.body,
-      isHtml: false,
-      app: "Gmail",
-    }
-    //Send a text message using default options
-    this.emailComposer.open(email);
-  }
 
 }

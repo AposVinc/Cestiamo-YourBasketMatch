392c4b81da1c646b164c11cd03f568a4c1b5b3f7
franfeg <francescofegatilli@gmail.com>
2020-01-14 13:01:20 +0100

    ProfiloPersonale, ProfiloUtente e Identazione
    ProfiloPersonale = aggiunta della mail
    ProfiloUtente = bottone per votare

--- src/services/partita.service.ts
+++ src/services/partita.service.ts
@@ -1,11 +1,11 @@
 import {HttpClient, HttpParams, HttpResponse} from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { Storage } from '@ionic/storage';
-import { URL } from '../constants';
-import { Partita } from '../model/partita.model';
-import { Campo } from "../model/campo.model";
-import {Tipopartita } from "../model/tipopartita.model";
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {Storage} from '@ionic/storage';
+import {URL} from '../constants';
+import {Partita} from '../model/partita.model';
+import {Campo} from "../model/campo.model";
+import {Tipopartita} from "../model/tipopartita.model";
 import {Utente} from "../model/utente.model";
 
 @Injectable()
@@ -15,7 +15,7 @@ export class PartitaService {
   }
 
   create(p: Partita, creatore: Utente) {
-    let body={p , creatore};
+    let body = {p, creatore};
     console.log(body);
     /*return this.http.post<Partita>(URL.NUOVA_PARTITA, p).toPromise()
       .then((response: Partita) => {
@@ -31,11 +31,12 @@ export class PartitaService {
     return this.http.get<Array<Partita>>(URL.LISTA_PARTITE);
   }
 
-  listMiePartite(utenteEmail : String): Observable<Array<Partita>> {
+  listMiePartite(utenteEmail: String): Observable<Array<Partita>> {
     let Url = `${URL.MIE_PARTITE}/utente=${utenteEmail}`;
     return this.http.get<Array<Partita>>(Url);
   }
-  listMiePartiteGiocate(utenteEmail : String): Observable<Array<Partita>> {
+
+  listMiePartiteGiocate(utenteEmail: String): Observable<Array<Partita>> {
     let Url = `${URL.PARTITE_GIOCATE}/utente=${utenteEmail}`;
     return this.http.get<Array<Partita>>(Url);
   }
@@ -45,27 +46,25 @@ export class PartitaService {
     return this.http.get<Partita>(apiURL);
   }
 
-  listCampi(): Observable<Array<Campo>>{
-    return this.http.get<Array<Campo>> (URL.LISTA_CAMPI)
+  listCampi(): Observable<Array<Campo>> {
+    return this.http.get<Array<Campo>>(URL.LISTA_CAMPI)
   }
 
-  ListTypeMatch(): Observable<Array<Tipopartita>>{
-    return this.http.get<Array<Tipopartita>> (URL.LISTA_TIPO_PARTITA)
+  ListTypeMatch(): Observable<Array<Tipopartita>> {
+    return this.http.get<Array<Tipopartita>>(URL.LISTA_TIPO_PARTITA)
   }
 
-  addUtente(partitaId: number, utenteEmail: string){
-    let url =  `${URL.ADD_PARTECIPANTE}/partita=${partitaId}/utente=${utenteEmail}`;
-    return this.http.put<Partita>(url,null,{ observe: 'response' })
-      .map((resp :HttpResponse<Partita>)=> {
+  addUtente(partitaId: number, utenteEmail: string) {
+    let url = `${URL.ADD_PARTECIPANTE}/partita=${partitaId}/utente=${utenteEmail}`;
+    return this.http.put<Partita>(url, null, {observe: 'response'})
+      .map((resp: HttpResponse<Partita>) => {
         return resp.body;
       });
   }
 
-  removeUtente(partitaId: number, utenteEmail: string){
+  removeUtente(partitaId: number, utenteEmail: string) {
     let deleteUrl = `${URL.REMOVE_PARTECIPANTE}/partita=${partitaId}/utente=${utenteEmail}`;
     return this.http.delete<Partita>(deleteUrl);
   }
 
-
 }
-

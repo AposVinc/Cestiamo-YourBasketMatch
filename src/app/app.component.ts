165f7d6e14b54ae052813ccddd25b493dc2400fa
AposVinc <apos.vincenzo@gmail.com>
2020-01-23 16:28:17 +0100

    profilo personale prende da storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -135,7 +135,7 @@ export class MyApp {
 
   openProfile() {//per entrare nel profilo dal menu laterale
     if (this.global.isLogged){
-      this.nav.push(PROFILO_PERSONALE_PAGE, { utenteEmail: this.utente.email});
+      this.nav.push(PROFILO_PERSONALE_PAGE);
       console.log('profilo utente', this.utente.nome, this.utente.cognome);
     } else {
       this.nav.push(LOGIN_PAGE);

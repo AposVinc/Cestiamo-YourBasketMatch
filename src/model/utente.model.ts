62c5d41ce0540de5c4a57affdd2eb9e157a042b8
AposVinc <apos.vincenzo@gmail.com>
2020-01-23 21:03:53 +0100

    Update utente.model.ts

--- src/model/utente.model.ts
+++ src/model/utente.model.ts
@@ -6,10 +6,10 @@ export class Utente {
   email: string;
   password: string;
   citta: string;
+  via: string;
   dataNascita: Date;
   numPartite: number;
   mediaVoto: Voto;
   img: string = "";
-  via: string;
 
 }
